d87fe3abd38b5cc9552ec07b4e715eadbd07f3e8
sempeck <jarek.sepek@mediorama.pl>
2015-10-21 19:48:43 +0200

    Loop wczytywania odległości

--- komiwojazer.pas
+++ komiwojazer.pas
@@ -1,82 +1,83 @@
 program komiwojazer;
 
 var
-	x, i : Integer;
-
+	x, i, j, odl : Integer;
 	odleglosc : array[1..10, 1..10] of Integer;
-    
     miasto : array[1..10] of String;
-
-	// AB, AC, AD, AE : Integer;
-	// BA, BC, BD, BE : Integer;
-	// CA, CB, CD, CE : Integer;
-    // DA, DB, DC, DE : Integer;
-    // EA, EB, EC, ED : Integer;
-
-
-procedure droga();
-  begin
-// 	for i := 1 to x do:
-// 		wyklucz miejsca gdzie byłeś
-// 		sprawdź najmniejsze odległości do innych 
-// 		wybierz najmniejszą i do niej przejdź. 
-// 		zaznacz, że tu byłeś (visited := true)
-//      zapisz do kolejności odwiedzania, trasy (dodaj do tablicy = droga?)
-//      dodaj liczbę przebytych kilometrów
-
-  end;
-
 
 begin
 	
-//zerowanie tablic
+//zerowanie tablic i integerów
+    x := 0;
+    i := 0;
+
     for i := 1 to 10 do
       begin
         odleglosc[i, i] := 0;
         miasto[i] := '';
-    end;
+      end;
 
 
 //początek
-	writeln('Ten program rozwiaze problem komiwojazera.');
+	writeln('Ten program znajdzie optymalna trase.');
     writeln('Przez ile miast musisz przejechac? Maksymalnie 10:');
     readln(x);   
 
 
-//wczytywanie kolejnych miast
+//wczytywanie kolejnych miast do tablicy
     for i := 1 to x do
       begin
 	      writeln('Podaj miasto numer ', i);
-	      readln(miasto[i]); // zapisuje na tablicy każde miasto....
-          
+	      readln(miasto[i]); // zapisuje na tablicy kolejne miasta
       end;
 
 
-    // ***** test czy miasta się wrzucają do tablicy ****
-    // write('Podales: '); 
-    // for i := 1 to x do write(miasto[i], ', ');
-    // **************************************************
-
-
+  
 //wczytywanie odległości
-    writeln(); //podawanie odległości
-    readln();
+    writeln('Podaj odleglosc:');
+ 
+    for j :=1 to x do
+	    begin
+	    for i := 1 to x do
+		  begin
+		 	writeln(miasto[i], ' - ', miasto[j]);
+		    readln(odl);
+            odleglosc[i, j] := odl;
+		  end;
+	    end;
+    
 
-      // podaj odległość AB AC AD AE ...
-      //                 BC BD BE    ...
-      //                 CD CE       ...
-      //                 DE          ...
+            
+
+
+//test
+     writeln('test ', odleglosc[1, 2]);
+
+      // Zderz każde miasto z każdym i wynik wpisz do tabeli. 
+      // Gdy pytasz o miasto "a" i "b", sprawdź czy pytałeś już o "b" i "a", jeśli tak, to pomiń i przepisz wynik z poprzedniego pytania. 
+      // Nie pytaj o "a" i "a", tylko wpisz 0.
+
 
 //od którego zacząć
-    writeln('Od ktorego miasta chcesz zaczac?');
-    writeln(); //rozpisanie case-ów A: .....
-
-//wywołanie procedury x razy
-droga;
+    writeln('Od ktorego miasta chcesz zaczac? Podaj numer:');
+    for i := 1 to x do writeln(i, ') ', miasto[i]);
+    readln;
 
 
-//po procedurze
-   // wróć do miasta początkowego
+//LICZENIE METODĄ GREEDY, czyli najbliższe miasto
+	// 	for i := 1 to x do:
+	//      
+	// 		wyklucz miejsca gdzie byłeś
+	// 		sprawdź najmniejsze odległości do innych 
+	// 		wybierz najmniejszą i do niej przejdź. 
+	// 		zaznacz, że tu byłeś (visited := true) || wywal z tablicy?
+	//      zapisz do kolejności odwiedzania, trasy (dodaj do tablicy = droga?)
+	//      dodaj liczbę przebytych kilometrów
+
+
+
+//po obliczeniach
+   // wróć do miasta początkowego i dodaj go do statystyk
 
 
 //WYNIKI

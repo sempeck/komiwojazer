673b4e7c8c485a75feaf4d000ce6c9fec3d015f6
sempeck <jarek.sepek@mediorama.pl>
2015-10-21 15:39:00 +0200

    wczytywaniemiast do tablicy

--- komiwojazer.pas
+++ komiwojazer.pas
@@ -1,42 +1,71 @@
 program komiwojazer;
 
 var
-	x : Integer;
+	x, i : Integer;
+
+	odleglosc : array[1..10, 1..10] of Integer;
+    
+    miasto : array[1..10] of String;
+
+	// AB, AC, AD, AE : Integer;
+	// BA, BC, BD, BE : Integer;
+	// CA, CB, CD, CE : Integer;
+    // DA, DB, DC, DE : Integer;
+    // EA, EB, EC, ED : Integer;
 
 
 procedure droga();
   begin
-// 	x razy powtórz procedurę:
+// 	for i := 1 to x do:
 // 		wyklucz miejsca gdzie byłeś
 // 		sprawdź najmniejsze odległości do innych 
 // 		wybierz najmniejszą i do niej przejdź. 
 // 		zaznacz, że tu byłeś (visited := true)
-// ***     zapisz do kolejności odwiedzania, trasy (dodaj do tablicy = droga?)
-//         dodaj liczbę przebytych kilometrów
+//      zapisz do kolejności odwiedzania, trasy (dodaj do tablicy = droga?)
+//      dodaj liczbę przebytych kilometrów
 
   end;
 
 
 begin
+	
+//zerowanie tablic
+    for i := 1 to 10 do
+      begin
+        odleglosc[i, i] := 0;
+        miasto[i] := '';
+    end;
+
+
+//początek
 	writeln('Ten program rozwiaze problem komiwojazera.');
-    writeln('Ile miast musisz odwiedzić?');
-    readln(x);
+    writeln('Przez ile miast musisz przejechac? Maksymalnie 10:');
+    readln(x);   
+
 
 //wczytywanie kolejnych miast
-    for x := 1 to x do
+    for i := 1 to x do
       begin
-	      writeln('Podaj miasto numer '); //numer miasta z pętli
-	      readln(); //zapisz zmienną każdego miasta....
+	      writeln('Podaj miasto numer ', i);
+	      readln(miasto[i]); // zapisuje na tablicy każde miasto....
+          
       end;
+
+
+    // ***** test czy miasta się wrzucają do tablicy ****
+    // write('Podales: '); 
+    // for i := 1 to x do write(miasto[i], ', ');
+    // **************************************************
+
 
 //wczytywanie odległości
     writeln(); //podawanie odległości
     readln();
 
-      // podaj odległość AB AC AD ...
-      //           BC BD    ...
-      //           CD       ...
-      //           ...
+      // podaj odległość AB AC AD AE ...
+      //                 BC BD BE    ...
+      //                 CD CE       ...
+      //                 DE          ...
 
 //od którego zacząć
     writeln('Od ktorego miasta chcesz zaczac?');

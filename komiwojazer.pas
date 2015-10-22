63eb892ed9c5b4ff76a5d2e21876441c156b0d0e
sempeck <jarek.sepek@mediorama.pl>
2015-10-22 11:12:34 +0200

    małe porządki

--- komiwojazer.pas
+++ komiwojazer.pas
@@ -2,8 +2,9 @@ program komiwojazer;
 
 var
 	x, i, j, odl : Integer;
-	odleglosc : array[1..10, 1..10] of Integer;
-    miasto : array[1..10] of String;
+	   odleglosc : array[1..10, 1..10] of Integer;
+        miasto : array[1..10] of String;
+
 
 begin
 	
@@ -19,7 +20,7 @@ begin
 
 
 //początek
-	writeln('Ten program znajdzie optymalna trase.');
+	  writeln('Ten program znajdzie optymalna trase.');
     writeln('Przez ile miast musisz przejechac? Maksymalnie 10:');
     readln(x);   
 
@@ -32,40 +33,38 @@ begin
       end;
 
 
-  
 //wczytywanie odległości
     writeln('Podaj odleglosc:');
  
-    for j :=1 to x do
+    for j := 1 to x do
 
 	    begin
 
-	    for i := 1 to x do
-		  begin
-		  
-            if (i = j) then
-              begin
-                odleglosc[i, j] := 0;
-                odleglosc[j, i] := 0;              	
-              end
+  	    for i := 1 to x do
+  		  begin
+  		  
+              if (i = j) then
+                begin
+                  odleglosc[i, j] := 0;
+                  odleglosc[j, i] := 0;              	
+                end
 
-            else
-                if (odleglosc[i, j] > 0) then odleglosc[i, j] := odleglosc[j, i] //gwarancja, że nie zapyta o ten sam dystans tylko w drugą stronę
-                // if (odleglosc[j, i] > 0) then odleglosc[j, i] := odleglosc[i, j];
-                else
-
-			 	begin
-				 	writeln(miasto[i], ' - ', miasto[j]);
-				    readln(odl);
-		            odleglosc[i, j] := odl;
-		            odleglosc[j, i] := odl;
-		        end;
-		  end;
+              else
+                  if (odleglosc[i, j] > 0) then odleglosc[i, j] := odleglosc[j, i] //gwarancja, że nie zapyta o ten sam dystans tylko w drugą stronę
+                  // if (odleglosc[j, i] > 0) then odleglosc[j, i] := odleglosc[i, j];
+                  else
+            			 	begin
+            				 	writeln(miasto[i], ' - ', miasto[j]);
+            				  readln(odl);
+            		      odleglosc[i, j] := odl;
+            		      odleglosc[j, i] := odl;
+            		    end;
+  		  end;
 	    end;
-    
-          
+       
 
 //test
+     Writeln('test miasta: ', miasto[1], ' - ', miasto[2], ' - ', miasto[3]);
      writeln('test 1-2: ', odleglosc[1, 2]);
      writeln('test 2-1: ', odleglosc[2, 1]);
      writeln('test 1-1: ', odleglosc[1, 1]);
@@ -77,8 +76,6 @@ begin
       readln;
 
 
-
-
 //LICZENIE METODĄ GREEDY, czyli najbliższe miasto
 	// 	for i := 1 to x do:
 	//      
@@ -86,9 +83,8 @@ begin
 	// 		sprawdź najmniejsze odległości do innych 
 	// 		wybierz najmniejszą i do niej przejdź. 
 	// 		zaznacz, że tu byłeś (visited := true) || wywal z tablicy?
-	//      zapisz do kolejności odwiedzania, trasy (dodaj do tablicy = droga?)
-	//      dodaj liczbę przebytych kilometrów
-
+	//    zapisz do kolejności odwiedzania, trasy (dodaj do tablicy = droga?)
+	//    dodaj liczbę przebytych kilometrów
 
 
 //po obliczeniach
